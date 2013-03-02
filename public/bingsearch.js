be8d6b3bf5acc0e4972181a62a3d66e4103d46ff
David Ascher <david.ascher@gmail.com>
2013-03-02 14:49:48 -0800

    / ‘public/bingsearch.js’

--- public/bingsearch.js
+++ public/bingsearch.js
@@ -1,23 +1,39 @@
-// bingsearch.js
-request = require('request');
-fs = require('fs');
+// // bingsearch.js
+// request = require('request');
+// fs = require('fs');
 
-function bing(term, top, skip, cb) {
-	// var ServiceRootURL = "https://api.duckduckgo.com";
-    var ServiceRootURL =  'https://api.datamarket.azure.com/Bing/Search/v1/Composite';
-    var SKEY = process.env['BING_APP_ID'];
-    console.log("SKEY", SKEY);
-    var params = {
-         Sources: "'web'", 
-         Query: "'"+term+"'", 
-         '$format': "JSON", 
-         '$top': top, '$skip': skip
-       },
-       req = request.get(ServiceRootURL).auth(SKEY, SKEY, false).qs(params);
-    request(req, cb)	
-}
+// function bing(term, top, skip, cb) {
+// 	// var ServiceRootURL = "https://api.duckduckgo.com";
+//     var ServiceRootURL =  'https://api.datamarket.azure.com/Bing/Search/v1/Composite';
+//     var SKEY = process.env['BING_APP_ID'];
+//     console.log("SKEY", SKEY);
+//     var params = {
+//          Sources: "'web'", 
+//          Query: "'"+term+"'", 
+//          '$format': "JSON", 
+//          '$top': top, '$skip': skip
+//        },
+//        req = request.get(ServiceRootURL).auth(SKEY, SKEY, false).qs(params);
+//     request(req, cb)	
+// }
 
-bing('tomato', 50, 0, function(err, response, body) {
-	fs.writeFileSync("bing.json", body)
-	console.log(JSON.parse(body));
-})
+// bing('tomato', 50, 0, function(err, response, body) {
+// 	fs.writeFileSync("bing.json", body)
+// 	console.log(JSON.parse(body));
+// })
+
+var restify = require('restify');
+
+// Creates a JSON client
+var client = restify.createJsonClient({
+  url: 'https://api.datamarket.azure.com',
+  // url: 'https://us-west-1.api.joyentcloud.com'
+});
+
+
+client.basicAuth(SKEY, SKEY);
+client.get('/Bing/Search/v1/Composite?q=tomato&$format=JSON&$top=10&$skip=0', function(err, req, res, obj) {
+  assert.ifError(err);
+
+  console.log(JSON.stringify(obj, null, 2));
+});
