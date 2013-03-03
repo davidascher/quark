966c2ae7565c52b96f191dc5a3ef8ede797f0e40
David Ascher <david.ascher@gmail.com>
2013-03-02 16:40:34 -0800

    / ‘client/pages.js’

--- client/pages.js
+++ client/pages.js
@@ -77,7 +77,7 @@ Template.main.rendered = function() {
 
 Template.main.pagestack = function() {
   var stackIds = Session.get("stackIds");
-  stackIds = ['a', 'b,' 'c'];
+  var stackIds = ['a', 'b,' 'c'];
   if (!stackIds) return;
   var pages = new Meteor.Collection();
   for (var i = 0; i < stackIds.length; i++) {
