8c2b94c18d4238b150b5334f1fd1f994ffc70a69
David Ascher <david.ascher@gmail.com>
2013-03-04 15:47:56 -0800

    / ‘client/pages.js’

--- client/pages.js
+++ client/pages.js
@@ -9,16 +9,18 @@ var Paras = new Meteor.Collection("paras");
 var Redirects = new Meteor.Collection("redirects");
 var showdown;
 var parasHandle = null;
+var pagesHandle = null;
 
 Meteor.autorun(function () {
   // always subscribe to the paras for the current page.
   var stackIds = Session.get("idStack");
   if (!stackIds) return;
   var id = stackIds[stackIds.length-1];
-  parasHandle = Meteor.subscribe('paras', {'page':id}, function() {
+  parasHandle = Meteor.subscribe('paras', {'page':id});
+  pagesHandle = Meteor.subscribe("pages", function() {
     console.log('setting idStack');
     Session.set("idStack", ["Welcome"]);
-  });
+  })
 });
 
 function doSearch(searchterm) {
