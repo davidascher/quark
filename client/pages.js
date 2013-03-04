bdf5c60d883762598d4935fdfbd973e9e6941949
David Ascher <david.ascher@gmail.com>
2013-03-04 11:26:27 -0800

    / ‘client/pages.js’

--- client/pages.js
+++ client/pages.js
@@ -259,6 +259,7 @@ Template.editablepagetitle.editing_title = function() {
 var endPagetitleEditing = function(evt, tmpl) {
   evt.stopPropagation();
   evt.preventDefault();
+  console.log(evt.target);
   Session.set("editing_title", null);
   var pageId = $(evt.target.parentNode).attr('data-id');
   console.log(pageId);
