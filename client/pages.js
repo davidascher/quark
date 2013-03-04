4849f72e7485f28fb39b1577b94da2084151772d
David Ascher <david.ascher@gmail.com>
2013-03-04 15:56:17 -0800

    / ‘client/pages.js’

--- client/pages.js
+++ client/pages.js
@@ -16,11 +16,12 @@ Session.set("idStack", []); // default
 pagesHandle = Meteor.subscribe("pages", function() {
   console.log('in cb for pagesHandle');
   var stackIds = Session.get("idStack");
-  if (!stackIds) 
+  if (!stackIds) {
     parasHandle = null;
-  else
+  } else {
     var id = stackIds[stackIds.length-1];
     parasHandle = Meteor.subscribe('paras', {'page':id});
+  }
 })
 
 function doSearch(searchterm) {
