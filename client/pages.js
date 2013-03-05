cb0d3d4f98da9699ef6b2fab015923992894d62a
David Ascher <david.ascher@gmail.com>
2013-03-04 22:46:16 -0800

    / ‘client/pages.js’

--- client/pages.js
+++ client/pages.js
@@ -533,7 +533,7 @@ function setPage (unescapedPageName) {
   console.log("in setPage", stack, stack[stack.length - 1], page);
   console.log("TRANSITIONING, BABY");
   Session.set("transitioning", true);
-  Session.set("nextPage", unescapedPageName);
+  // Session.set("nextPage", unescapedPageName);
   return;
   if (!page) { // we don't have data yet, offer to create one
     var redirect = Redirects.findOne({old_name: unescapedPageName});
