835c78aebfc480d1faff258e47b736f9a1d78afe
David Ascher <david.ascher@gmail.com>
2013-03-04 21:00:10 -0800

    / ‘client/pages.js’

--- client/pages.js
+++ client/pages.js
@@ -19,6 +19,7 @@ parasHandle = Meteor.subscribe("paras"); // probably needs to be more efficient,
 Template.slider.helpers({
   currentScreen: function() {
     console.log('in currentScreen')
+    return 'page';
     x = Meteor.Transitioner.currentPage();
     console.log("CURRENSCREEN",x);
     return x;
@@ -565,7 +566,7 @@ function setPage (unescapedPageName) {
   stack.push(id);
   stack = stack.slice(-4);
   Session.set("idStack", stack)
-  return 'page';
+  return 'main';
 }
 
 Meteor.Router.add({
